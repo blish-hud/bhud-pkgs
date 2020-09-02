f829cf4fb1892a63608ccd06ae6c7b529de4f792
Dade Lamkins <dade@lamkins.org>
2020-09-01 23:00:25 -0400

    Changed compile-manifest.ps1 to build an array of manifests isntead of nested objects.

--- tools/compile-manifest.ps1
+++ tools/compile-manifest.ps1
@@ -1,18 +1,11 @@
 $PSDefaultParameterValues = @{ '*:Encoding' = 'utf8' }
 
-$manifest = @{}
+$manifest = [System.Collections.ArrayList]@()
 
 Get-ChildItem -Path .\ -Recurse -Filter *.json | ForEach-Object {
     $packageManifest = (Get-Content -Path $_.FullName | ConvertFrom-Json)
 
-    $namespace = $packageManifest.namespace
-    $version = $_.BaseName
-
-    if (!$manifest.ContainsKey($namespace)) {
-        $manifest.Add($namespace, @{})
-    }
-
-    $manifest[$namespace][$version] = $packageManifest
+    [void]$manifest.Add($packageManifest)
 }
 
 (ConvertTo-Json $manifest -Depth 8 -Compress) | Write-Host
